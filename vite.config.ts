1117a2ace4fd437c7c1e0bce39acda7b03d77f8f
Ian Forsyth <ian@waveform.com>
2026-04-16 16:37:47 -0700

    Build Front plugin page in production

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        frontPlugin: "front-plugin.html",
+      },
+    },
+  },
   server: {
     proxy: {
       "/api": "http://127.0.0.1:3001",
